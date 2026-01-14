9f2b92b515ce232e12e3851f6036a260432f6185
Himanshu-Suri <himanshusuri.2795@gmail.com>
2026-01-14 16:23:29 +0530

    changes to index.js yet again

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,26 +11,26 @@ import { app, server } from "./lib/socket.js";
 dotenv.config()
 const PORT = process.env.PORT
 const __dirname = path.resolve();
+
 app.use(express.json({limit: '100mb'}));
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+  origin: "http://localhost:5173",
+  credentials: true
 }))
 
-app.use("/api/auth" , authRoutes)
-app.use("/api/message" , messageRoute)
+app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoute)
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/my-react-app/dist")));
-
-  app.get("/:path(*)", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/my-react-app", "dist", "index.html"));
+  app.all("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/my-react-app/dist/index.html"));
   });
 }
 
-server.listen(PORT , () => {
-    console.log("Server is running!~")
-    connectDB();
+server.listen(PORT, () => {
+  console.log("Server is running!~")
+  connectDB();
 })
