fb164aa8507dfb65fd7fddc39cca6d1299d24897
Himanshu-Suri <himanshusuri.2795@gmail.com>
2026-01-14 16:43:14 +0530

    changes to db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-      const conn =  await mongoose.connect(process.env.MONGODB_URL);
-    console.log(`MonogoDB connected: ${conn.connection.host}`)
-    }
-    catch (err){
-    console.log(`MonogoDB connected: ${conn.connection.host}`)
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.log(`MongoDB connection error: ${err.message}`);
+    process.exit(1);
+  }
 }
